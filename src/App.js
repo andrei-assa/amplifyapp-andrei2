215d3ec9cb8f67d40591d5422e2bff965dafca3d
Andrei Assa <andrei.assa@gmail.com>
2021-07-13 15:21:51 -0400

    Update on dev branch

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-            Continuous integration is working!
+            This is on dev branch.
         </p>
         <a
           className="App-link"
